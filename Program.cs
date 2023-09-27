c88b31c0b4b4e1890dddd3b87e7e8a666685a82d
Polina Kostsova <kostsovapa@gmail.com>
2023-09-27 23:55:15 +0300

    Task13

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ System.Console.WriteLine("msg");
 return Convert.ToInt32(Console.ReadLine());
 }
 int number = Prompt("Введите число");
-
+int fnumber(int number)
 {
 while (number > 999)
 {
@@ -25,4 +25,4 @@ else return true;
 if (check(number) != true)
 System.Console.WriteLine("Третьей цифры нет");
 else
-System.Console.WriteLine("Третья цифра числа {number} является {fnumber(number)}");
+System.Console.WriteLine("Третья цифра числа {0} является {1}", number, fnumber(number));
