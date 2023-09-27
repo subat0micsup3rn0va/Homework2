64a16db064133cfa3b745a84d62ba91d8801f373
subat0micsup3rn0va <144050421+subat0micsup3rn0va@users.noreply.github.com>
2023-09-28 00:43:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿ //Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным:
+ int Prompt(string msg)
+{
+
+System.Console.WriteLine("msg");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string ss = "Не день недели";
+string check(int number)
+{
+if (number >= 6 && number < 8) ss = "Выходной";
+if (number >= 1 && number < 7) ss = "Будний день";
+return ss;
+}
+System.Console.WriteLine(check(number));
