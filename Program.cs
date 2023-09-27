d6b15ef4f9926e8627bbc21a8a242d6f66b42312
Polina Kostsova <kostsovapa@gmail.com>
2023-09-27 22:23:51 +0300

    task10

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа:
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($'{msg}');
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt('Введите трехзначное число');
+if (number >=100 && number <1000)
+{
+int mid = (number % 100 — number % 10) / 10;
+System.Console.WriteLine($'Вторая цифра числа {number} является {mid}');
+}
+else
+System.Console.WriteLine(«Это число не трехзначное»';
